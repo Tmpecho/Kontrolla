912b6feca1368feb141728513295e6c820970127
Tmpecho <TMPEcho@gmail.com>
2026-03-20 13:10:00 +0100

    chore(backend): configure spring foundation

--- backend/src/main/java/org/kontrolla/KontrollaApplication.java
+++ backend/src/main/java/org/kontrolla/KontrollaApplication.java
@@ -2,11 +2,13 @@ package org.kontrolla;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class KontrollaApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(KontrollaApplication.class, args);
 	}
 
