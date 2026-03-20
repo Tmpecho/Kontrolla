46c914b6802dcf9da9c95b0f98225375d1f2b1b7
Tmpecho <TMPEcho@gmail.com>
2026-03-20 13:27:25 +0100

    Add Java Spring Boot backend
    Signed-off-by: Tmpecho <TMPEcho@gmail.com>

---
+++ backend/src/main/java/org/kontrolla/KontrollaApplication.java
@@ -0,0 +1,13 @@
+package org.kontrolla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KontrollaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KontrollaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/kontrolla/KontrollaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kontrolla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KontrollaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
