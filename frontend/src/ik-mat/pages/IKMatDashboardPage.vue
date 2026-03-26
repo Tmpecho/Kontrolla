c75f7a45da08c324c0bd74ec2957458666264a9b
Tmpecho <TMPEcho@gmail.com>
2026-03-26 12:26:51 +0100

    feat(frontend): add IK-mat and IK-alkohol dashboards with navigation links in Workspace page

---
+++ frontend/src/ik-mat/pages/IKMatDashboardPage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section>
+    <h1>IK-alkohol Dashboard</h1>
+    <p>Overview over food compliance.</p>
+  </section>
+
+  <section>
+    <h2>Important</h2>
+    <p>Important things that need action.</p>
+  </section>
+</template>
