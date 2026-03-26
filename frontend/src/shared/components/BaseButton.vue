be29d7b11a702534b739ac3debcdd7180bf7a033
eilifhl <eilifhl@stud.ntnu.no>
2026-03-26 13:33:07 +0100

    Add more radius

--- frontend/src/shared/components/BaseButton.vue
+++ frontend/src/shared/components/BaseButton.vue
@@ -25,7 +25,7 @@ defineProps<{
   justify-content: space-between;
   align-items: center;
   transition: background-color 0.2s ease;
-  border-radius: 2px;
+  border-radius: 3px;
 }
 
 .button:hover {
